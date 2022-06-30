6f21147793a8464306d69173a6054bd8ef021649
Gabriel Rios Souza <RiosSoGa@BASF.onmicrosoft.com>
2022-06-30 14:52:11 -0300

    Starting porject with Vite using React and Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { LoginPage } from "./pages/LoginPage"
+
+export default function App() {
+  return (
+    <div className="app">
+      <LoginPage/>
+    </div>
+  )
+}
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,9 @@
+import { Header } from "../components/Header"
+
+export const LoginPage = () => {
+  return (
+    <>
+        <Header/>
+    </>
+    )
+}
